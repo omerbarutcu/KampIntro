ea1d7a7ecece41d6e16b88b180f1845e3ace5a3d
omerbarutcu <barolina@gmail.com>
2022-03-16 11:35:48 +0300

    5. gün sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseloggerService = new DatabaseLoggerService();
+            ILoggerService fileloggerService = new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> {databaseloggerService,fileloggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager,loggers);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager,tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
